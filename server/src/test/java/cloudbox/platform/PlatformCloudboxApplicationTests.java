0dcd583245c0f82228efc97bdee946919828be4e
hu.jian <hu.jian@winning.com.cn>
2026-02-13 23:33:43 +0800

    1

--- server/src/test/java/cloudbox/platform/PlatformCloudboxApplicationTests.java
+++ server/src/test/java/cloudbox/platform/PlatformCloudboxApplicationTests.java
@@ -3,7 +3,7 @@ package cloudbox.platform;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = PlatformCloudboxApplication.class)
 class PlatformCloudboxApplicationTests {
 
     @Test
